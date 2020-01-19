0b4d35ad664eb9c89cda775f69a6d94eb316ef30
Alexander <manaac@mail.ru>
2020-01-19 17:41:28 +0200

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,13 +26,12 @@ class ArticlesController < ApplicationController
   end
 
   def create
-      debugger
       #render plain: params[:article].inspect
       @article = Article.new(article_params)
       @article.user = current_user
       if @article.save
         flash[:notice] = "Article was successfuly created"
-        redirect_to article_path(@article)
+        redirect_to articles_path
       else
         render 'new'
       end
